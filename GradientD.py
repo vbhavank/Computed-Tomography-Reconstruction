Gradient bug fix

GradientD.py
@@ -1,3 +1,5 @@
+import pdb
+
 import scipy.io as sp
 import numpy as np
 import matplotlib.pyplot as plt
@@ -5,18 +7,27 @@ import matplotlib.pyplot as plt
 
 x = sp.loadmat("simulated_heart.mat")
 A_matrix = x['A']
+y_observation = x['y'][0, :]
+
 x_vec = x['x']
-y_observation = x['y']
-num_iteration = 1000
-verbose_step = 200
 
-x_init_value = y_observation[0, 0]/sum(A_matrix[0, :])
-x_location = np.where(A_matrix[0, :] == 1)
+# Generating new x vector
+g = np.random.randn(25)+ 5
+u = np.random.uniform(0, 10, 25)
+x = 0.2*g + u
+x_vec = x.flatten().reshape(1, 25)
+Ax = A_matrix@x_vec.flatten()
+y_observation = np.random.poisson(Ax).reshape(28)
 
+print("Image vector x is of shape {}".format(np.shape(x_vec)))
+print("Mixing matrix A is of shape {}".format(np.shape(A_matrix)))
+print("Obervation matrix Y is of shape {}".format(np.shape(y_observation)))
+
+num_iteration = 100000
+verbose_step = 2000
+
+# Initialize x with vector of all ones.
 x_init = np.ones(x_vec.shape)
-
-for x_lo in x_location:
-    x_init[0, x_lo] = x_init_value
 
 class GradientDescent():
     def __init__(self,
@@ -27,30 +38,32 @@ class GradientDescent():
 
     def compute_gradient(self):
         inner_term_final = 0
-        for i in range(len(self.y_obser[0, :])):
-            inner_term1 = ((self.y_obser[0, i] * self.A_mat[i, :]) / ((self.A_mat * self.x_old)[i, :]))
-            # Warning: Setting nan values to zero
-            inner_term1[np.isnan(inner_term1)] = 0
-            inner_term_final += (inner_term1 - self.A_mat[i, :])
+        Axn = (self.A_mat * self.x_old)
+        for i in range(len(self.y_obser)):
+            inner_term_final += (((self.y_obser[i] * self.A_mat[i, :]) / sum(Axn[i, :])) - self.A_mat[i, :])
         return inner_term_final
 
-    def _get_new_x(self, x_old, lr_r=0.0001):
+    def _get_new_x(self, x_old, lr_r=0.01):
         self.x_old = x_old
-        x_new = np.zeros(x_old.shape)
         gradient_step = self.compute_gradient()
-        for i, x_j in enumerate(x_old[0, :]):
-            x_new[0, i] = (x_j + lr_r*(gradient_step))[i]
+        x_new = (x_old + lr_r*(gradient_step))
         return x_new
 
 Gd_solver = GradientDescent(y_observation, A_matrix)
 print("Initialization: {}".format(x_init))
 for i in range(num_iteration):
     x_new = Gd_solver._get_new_x(x_init)
-    x_init = x_new
+    diff = np.linalg.norm(x_new - x_init)
+    mse = np.linalg.norm(x_new - x_vec)
     if i % verbose_step == 0:
-        print("After {} iteration: {}".format(i, x_new))
+        print(f'epoc: {i:2d}, diff: {diff: .4E}, mse: {mse:8.4f}')
+    if (diff <= 1e-5):
+        print(f'Convergence at: epoc: {i:2d}, diff: {diff: .4E}, mse: {mse:8.4f}')
+        break
+    x_init = x_new
+
 print("Actual x_vector simulated:{}".format(x_vec))
-mse = sum(((x_new - x_vec)**2)[0])
+print("Solution from Gradient Ascent:{}".format(x_new))
 print("MSE of solution:{}".format(mse))
 
 plt.figure()